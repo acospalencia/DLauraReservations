7b8e3cd3be6b83ac5b26b5c0754bbeb78aad5eb8
Acosta <osoloco1245@gmail.com>
2025-11-17 15:47:01 -0600

    some changes

--- DLauraWebApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DLauraWebApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
